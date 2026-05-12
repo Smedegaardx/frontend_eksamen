f4babfb21390fe45aea61f27f35b287bcc8349c3
Smedegaardx <anan0013@stud.kea.dk>
2026-05-12 14:52:49 +0200

    Forsøg på hover lavet

--- src/app/components/Herobtn.jsx
+++ src/app/components/Herobtn.jsx
@@ -3,13 +3,15 @@ const Herobtn = ({ text, color }) => {
     <button
       data-color={color}
       className="h-16 w-42 
-      pink:bg-linear-125 pink:from-[#b822b5] pink:to-[#F72A78]
-      standard:border-3 border-neutral-400"
+      pink:bg-linear-125 pink:from-[#b822b5] pink:via-50% pink:to-[#F72A78]
+      pink:hover:bg-none pink:hover:bg-[#b822b5] transition ease-in duration-200 transition-discrete
+      standard:border-3 standard:border-neutral-400"
     >
-      <div className="h-full w-full bg-linear-220 from-white/70 to-20% to-transparent p-0.75 standard:bg-none">
+      <div className="h-full w-full bg-linear-220 from-white/70 to-20% to-transparent p-0.75 standard:bg-none pink:hover:bg-linear-250">
         <div
           className="h-full w-full flex justify-center items-center 
-        pink:bg-linear-125 pink:from-[#b822b5] pink:to-[#F72A78]
+        pink:bg-linear-125 pink:from-[#b822b5] pink:via-50% pink:to-[#F72A78]
+        pink:hover:bg-none pink:hover:bg-[#b822b5] transition-all ease-in duration-500 transition-discrete
         "
         >
           <p className="text-lg uppercase tracking-widest">{text}</p>
