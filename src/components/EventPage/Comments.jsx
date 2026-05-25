29d8e5d9f34720ce21ae95470d2b9e62035629d8
Smedegaardx <anan0013@stud.kea.dk>
2026-05-25 14:31:01 +0200

    Date formatting

--- src/components/EventPage/Comments.jsx
+++ src/components/EventPage/Comments.jsx
@@ -6,7 +6,9 @@ const Comments = ({ API }) => {
   return (
     <div className="bg-[url(/assets/bg/pattern_bg.jpg)]">
       <div className="mx-auto max-w-400 py-10 ">
-        <h1 className="uppercase text-5xl font-extrabold">X comments</h1>
+        <h1 className="uppercase text-5xl font-extrabold">
+          {API.length} comments
+        </h1>
         {API.map((comment) => (
           <SingleComment comment={comment} key={comment.id}></SingleComment>
         ))}
