26adcb3b0cd3497aeda13ab6cd75e838e4855776
Smedegaardx <anan0013@stud.kea.dk>
2026-05-26 22:19:27 +0200

    Added music to assets, and made an array for the songs

--- src/components/index/TrackSection.jsx
+++ src/components/index/TrackSection.jsx
@@ -1,6 +1,27 @@
+"use client";
+
+import { useRef, useState } from "react";
 import CurrentTrack from "./CurrentTrack";
 import Sectionheader from "./Sectionheader";
 import TrackSelector from "./TrackSelector";
+
+const tracks = [
+  {
+    title: "Black Box Funky",
+    src: "/assets/songs/black-box-funky.mp3",
+    cover: "/assets/content-img/track1.jpg",
+  },
+  {
+    title: "Euphoria",
+    src: "/assets/songs/euphoria.mp3",
+    cover: "/assets/content-img/track2.jpg",
+  },
+  {
+    title: "Fashion Red Tape",
+    src: "/assets/songs/fashion-red-tape.mp3",
+    cover: "/assets/content-img/track4.jpg",
+  },
+];
 
 const TrackSection = () => {
   return (
