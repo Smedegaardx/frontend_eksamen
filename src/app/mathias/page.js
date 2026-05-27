d64f69f262b99ed615ca1957ab67f94e996fca13
Mathias <math86s0@stud.kea.dk>
2026-05-28 01:37:40 +0200

    Færdigt newsletter component inklusiv validering

--- src/app/mathias/page.js
+++ src/app/mathias/page.js
@@ -3,15 +3,13 @@ import Button from "@/components/Button";
 import Siteheader from "@/components/Siteheader";
 import Welcome from "@/components/index/Welcome";
 import Sectionheader from "@/components/index/Sectionheader";
+import Newsletter from "@/components/index/Newsletter";
 
 export default function Home() {
   return (
     <div>
-      {/* <Footer></Footer> */}
-      {/* <Button buttonText="Read More"></Button> */}
-      <Welcome></Welcome>
-      <Siteheader title="This is a site header"></Siteheader>
-      <Sectionheader title="This is a section header"></Sectionheader>
+      <Newsletter />
+      <Footer />
     </div>
   );
 }
