7ea5066458baaef1f7411d6bcd380019fee38270
Smedegaardx <anan0013@stud.kea.dk>
2026-05-28 06:39:28 +0200

    Featured events made responsive

--- src/components/index/Sectionheader.jsx
+++ src/components/index/Sectionheader.jsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 const Sectionheader = ({ title }) => {
   return (
     <div className="flex flex-col items-center mt-8 mb-8">
-      <h1 className="text-4xl uppercase tracking-wide">{title}</h1>
+      <h1 className="text-3xl md:text-4xl uppercase tracking-wide mb-2 text-center">
+        {title}
+      </h1>
       <Image
         src="/assets/bottom_line2.png"
         alt="Line decoration"
