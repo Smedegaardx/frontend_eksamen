447ce4f8014673683fbe9c3948fa3ccd65c3ca68
Mathias <math86s0@stud.kea.dk>
2026-05-28 17:08:00 +0200

    Har tilføjet oversigt over bordstatus, samt svg'er der passer

--- src/components/Booking/BookingForm.jsx
+++ src/components/Booking/BookingForm.jsx
@@ -7,34 +7,48 @@ const BookingForm = () => {
       <form className="grid grid-cols-2 gap-5 mt-10">
         <input
           type="text"
-          placeholder="Name"
-          className="w-full border border-white text-white placeholder:text-white/80 focus:outline-none focus:border-brand focus:ring-1 focus:ring-brand p-3"
+          placeholder="Your Name"
+          className="w-full border border-white text-white placeholder:text-white focus:outline-none focus:border-brand focus:ring-1 focus:ring-brand p-3"
         ></input>
+
         <input
           type="email"
-          placeholder="Email"
-          className="w-full border border-white text-white placeholder:text-white/80 focus:outline-none focus:border-brand focus:ring-1 focus:ring-brand p-3"
+          placeholder="Your Email"
+          className="w-full border border-white text-white placeholder:text-white focus:outline-none focus:border-brand focus:ring-1 focus:ring-brand p-3"
         ></input>
+
         <input
           type="text"
-          placeholder="Table Number"
-          className="w-full border border-white text-white placeholder:text-white/80 focus:outline-none focus:border-brand focus:ring-1 focus:ring-brand p-3"
+          placeholder="Your Phone Number"
+          className="w-full border border-white text-white placeholder:text-white focus:outline-none focus:border-brand focus:ring-1 focus:ring-brand p-3"
         ></input>
-        <input
-          type="text"
-          placeholder="Number of Guests"
-          className="w-full border border-white text-white placeholder:text-white/80 focus:outline-none focus:border-brand focus:ring-1 focus:ring-brand p-3"
-        ></input>
-        <input
-          type="text"
-          placeholder="Event"
-          className="w-full border border-white text-white placeholder:text-white/80 focus:outline-none focus:border-brand focus:ring-1 focus:ring-brand p-3"
-        ></input>
-        <input
-          type="text"
-          placeholder="Phone Number"
-          className="w-full border border-white text-white placeholder:text-white/80 focus:outline-none focus:border-brand focus:ring-1 focus:ring-brand p-3"
-        ></input>
+
+        <select className="w-full border border-white text-white placeholder:text-white focus:outline-none focus:border-brand focus:ring-1 focus:ring-brand p-3">
+          <option value="">Choose Event</option>
+          <option value="event1">Event 1</option>
+          <option value="event2">Event 2</option>
+        </select>
+
+        <select className="w-full border border-white text-white placeholder:text-white focus:outline-none focus:border-brand focus:ring-1 focus:ring-brand p-3">
+          <option value="">Choose Number of Guests</option>
+          <option value="1">1 Guest</option>
+          <option value="2">2 Guests</option>
+          <option value="3">3 Guests</option>
+          <option value="4">4 Guests</option>
+          <option value="5">5 Guests</option>
+          <option value="6">6 Guests</option>
+          <option value="7">7 Guests</option>
+          <option value="8">8 Guests</option>
+        </select>
+
+        <select className="w-full border border-white text-white placeholder:text-white focus:outline-none focus:border-brand focus:ring-1 focus:ring-brand p-3">
+          <option value="">Table Number</option>
+          <option value="1">Table 1</option>
+          <option value="2">Table 2</option>
+          <option value="3">Table 3</option>
+          <option value="4">Table 4</option>
+        </select>
+
         <textarea
           placeholder="Additional Notes"
           className="w-full border border-white text-white placeholder:text-white/80 focus:outline-none focus:border-brand focus:ring-1 focus:ring-brand p-3 min-h-48 resize-y col-span-2"
--- src/components/Booking/TableBooking.jsx
+++ src/components/Booking/TableBooking.jsx
@@ -2,23 +2,54 @@ import Image from "next/image";
 
 const TableBooking = () => {
   return (
-    <div className="hidden w-full md:grid md:grid-cols-5 gap-y-10 items-center justify-items-center">
-      <img src="/assets/table/table_1.svg" alt="Small table" className="" />
-      <img src="/assets/table/table_1.svg" alt="Small table" className="" />
-      <img src="/assets/table/table_2.svg" alt="Small table" className="" />
-      <img src="/assets/table/table_1.svg" alt="Small table" className="" />
-      <img src="/assets/table/table_3.svg" alt="Small table" className="" />
-      <img src="/assets/table/table_1.svg" alt="Small table" className="" />
-      <img src="/assets/table/table_1.svg" alt="Small table" className="" />
-      <img src="/assets/table/table_2.svg" alt="Small table" className="" />
-      <img src="/assets/table/table_1.svg" alt="Small table" className="" />
-      <img src="/assets/table/table_3.svg" alt="Small table" className="" />
-      <img src="/assets/table/table_1.svg" alt="Small table" className="" />
-      <img src="/assets/table/table_1.svg" alt="Small table" className="" />
-      <img src="/assets/table/table_2.svg" alt="Small table" className="" />
-      <img src="/assets/table/table_1.svg" alt="Small table" className="" />
-      <img src="/assets/table/table_3.svg" alt="Small table" className="" />
-    </div>
+    <article>
+      <div className="hidden w-full md:grid md:grid-cols-5 gap-y-10 items-center justify-items-center">
+        <img src="/assets/table/table_1.svg" alt="Small table" className="" />
+        <img src="/assets/table/table_1.svg" alt="Small table" className="" />
+        <img src="/assets/table/table_2.svg" alt="Small table" className="" />
+        <img src="/assets/table/table_1.svg" alt="Small table" className="" />
+        <img src="/assets/table/table_3.svg" alt="Small table" className="" />
+
+        <img src="/assets/table/table_1.svg" alt="Small table" className="" />
+        <img src="/assets/table/table_1.svg" alt="Small table" className="" />
+        <img src="/assets/table/table_2.svg" alt="Small table" className="" />
+        <img src="/assets/table/table_1.svg" alt="Small table" className="" />
+        <img src="/assets/table/table_3.svg" alt="Small table" className="" />
+
+        <img src="/assets/table/table_1.svg" alt="Small table" className="" />
+        <img src="/assets/table/table_1.svg" alt="Small table" className="" />
+        <img src="/assets/table/table_2.svg" alt="Small table" className="" />
+        <img src="/assets/table/table_1.svg" alt="Small table" className="" />
+        <img src="/assets/table/table_3.svg" alt="Small table" className="" />
+      </div>
+
+      <div className="flex gap-15 mt-15 justify-center">
+        <div className="flex flex-col items-center gap-2">
+          <img
+            src="/assets/table/table_1.svg"
+            alt="Small table"
+            className="h-16 w-16"
+          />
+          <p>Available</p>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <img
+            src="/assets/table/table_1_booked.svg"
+            alt="Small table"
+            className="h-16 w-16"
+          />
+          <p>Reserved</p>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <img
+            src="/assets/table/table_1_selected.svg"
+            alt="Small table"
+            className="h-16 w-16"
+          />
+          <p>Selected</p>
+        </div>
+      </div>
+    </article>
   );
 };
 
