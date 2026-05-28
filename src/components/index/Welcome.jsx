7208a564a42cc49954f6d2e1988915aa26c1d42f
Anthon <anan0013@stud.kea.dk>
2026-05-28 17:56:58 +0200

    Animation made for welcome section

--- src/components/index/Welcome.jsx
+++ src/components/index/Welcome.jsx
@@ -2,6 +2,8 @@
 
 import Sectionheader from "./Sectionheader";
 import { motion } from "framer-motion";
+import { LuConciergeBell } from "react-icons/lu";
+import { PiCheersFill } from "react-icons/pi";
 
 const services = [
   {
@@ -13,134 +15,151 @@ const services = [
   },
   {
     title: "Restaurant",
-    thumbnail: "/assets/content-img/reastaurant_1.jpg",
+    thumbnail:
+      "/assets/content-img/reastaurant_1.jpg",
     description:
       "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution.",
-    icon: "/assets/icon/favicon.png",
+    icon: <LuConciergeBell />,
   },
   {
     title: "Bar",
     thumbnail: "/assets/content-img/thumb2.jpg",
     description:
       "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin.",
-    icon: "/assets/icon/favicon.png",
+    icon: <PiCheersFill />,
   },
 ];
 
-const overlayVariants = {
-  hidden: { opacity: 0 },
-  visible: { opacity: 1, transition: { duration: 1.5, ease: "easeInOut" } },
-};
-
 const Welcome = () => {
   return (
-    <section className="bg-[url(/assets/bg/pattern_bg.jpg)] bg-cover bg-center mb-8">
+    <section className="bg-[url(/assets/bg/pattern_bg.jpg)] bg-cover bg-center py-20">
       <Sectionheader title="Welcome in nightclub" />
-      <div className="grid grid-cols-3 max-w-4xl mx-auto">
+      <div className="grid grid-cols-3 gap-6 max-w-6xl mx-auto">
         {services.map((service, i) => (
           <motion.div
             key={i}
-            className="relative overflow-hidden aspect-3/4 cursor-pointer"
-            whileHover="visible"
-            initial="hidden"
+            className="relative aspect-3/4 grid grid-cols-1 grid-rows-1"
+            initial="rest"
+            whileHover="hover"
           >
             <motion.img
               src={service.thumbnail}
               alt={service.title}
-              className="w-full h-full object-cover"
-              variants={{
-                hidden: { scale: 1 },
-                visible: {
-                  scale: 1.05,
-                  transition: { duration: 1.5, ease: "easeInOut" },
-                },
-              }}
+              className="w-full h-full object-cover col-start-1 row-start-1"
             />
 
             {/* Sort */}
             <motion.div
-              className="absolute inset-0 bg-black/75 flex flex-col items-center justify-center p-6"
-              variants={overlayVariants}
+              className="col-start-1 row-start-1 w-full h-full bg-black flex flex-col items-center justify-center 
+              
+              border-t border-b border-(--color-brand)
+
+              before:content-[''] before:absolute before:top-0 before:left-0
+              before:w-20 before:h-20 before:bg-(--color-brand)
+              before:[clip-path:polygon(0_0,100%_0,0_100%)]
+
+              after:content-[''] after:absolute after:bottom-0 after:right-0
+              after:w-20 after:h-20 after:bg-(--color-brand)
+              after:[clip-path:polygon(100%_0,100%_100%,0_100%)]"
+              variants={{
+                rest: {
+                  opacity: 0,
+                },
+                hover: {
+                  opacity: 1,
+
+                  transition: {
+                    duration: 0.3,
+                  },
+                },
+              }}
             >
-              <motion.span
-                className="absolute top-0 left-0 w-10 h-10 border-t-4 border-l-4 border-pink-600"
-                variants={{
-                  hidden: { opacity: 0, x: -10, y: -10 },
-                  visible: {
-                    opacity: 1,
-                    x: 0,
-                    y: 0,
-                    transition: { duration: 1.5, ease: "easeOut" },
-                  },
-                }}
-              />
-
-              {/* Hjørner */}
-              <motion.span
-                className="absolute bottom-0 right-0 w-10 h-10 border-b-4 border-r-4 border-pink-600"
-                variants={{
-                  hidden: { opacity: 0, x: 10, y: 10 },
-                  visible: {
-                    opacity: 1,
-                    x: 0,
-                    y: 0,
-                    transition: { duration: 1.5, ease: "easeOut" },
-                  },
-                }}
-              />
-
               {/* Ikon */}
               <motion.div
-                className="w-16 h-16 border-2 border-pink-600 flex items-center justify-center mb-4 text-2xl text-pink-500"
+                className="w-24 h-24 border-2 border-(--color-brand) flex items-center justify-center mb-4 text-2xl text-(--color-brand) rounded"
                 variants={{
-                  hidden: { opacity: 0, scale: 0.8 },
-                  visible: {
+                  rest: {
+                    opacity: 0,
+                    scale: 0.2,
+                  },
+                  hover: {
                     opacity: 1,
                     scale: 1,
-                    transition: { duration: 1.5, ease: "easeOut" },
+                    transition: {
+                      duration: 0.3,
+                      ease: "easeOut",
+                    },
                   },
                 }}
               >
-                {service.icon}
+                {i === 0 ? (
+                  <motion.img
+                    src={service.icon}
+                    alt={service.title}
+                    className="w-12 h-12 stroke-(--color-brand)"
+                    variants={{
+                      rest: { scale: 0.2 },
+                      hover: {
+                        scale: 1.05,
+                        transition: {
+                          duration: 0.3,
+                          ease: "easeInOut",
+                        },
+                      },
+                    }}
+                  />
+                ) : (
+                  <motion.div
+                    className="scale-180"
+                    variants={{
+                      rest: { scale: 0.2 },
+                      hover: {
+                        scale: 1.05,
+                        transition: {
+                          duration: 0.3,
+                          ease: "easeInOut",
+                        },
+                      },
+                    }}
+                  >
+                    {service.icon}
+                  </motion.div>
+                )}
               </motion.div>
 
               {/* Title */}
               <motion.h3
-                className="text-white text-xs font-bold tracking-widest uppercase mb-3"
+                className="text-white text-lg font-bold tracking-widest uppercase mb-6"
                 variants={{
-                  hidden: { opacity: 0, y: 10 },
-                  visible: {
+                  rest: {
+                    opacity: 0,
+                    scale: 1,
+                  },
+                  hover: {
                     opacity: 1,
-                    y: 0,
-                    transition: { duration: 1.5, ease: "easeOut" },
+                    scale: 1.2,
+                    transition: {
+                      duration: 1,
+                      ease: "easeOut",
+                    },
                   },
                 }}
               >
                 {service.title}
               </motion.h3>
 
-              {/* Divider */}
-              <motion.div
-                className="h-0.5 bg-pink-600 mb-3"
-                variants={{
-                  hidden: { width: 0, opacity: 0 },
-                  visible: {
-                    width: 40,
-                    opacity: 1,
-                    transition: { duration: 1.5, ease: "easeOut" },
-                  },
-                }}
-              />
-
               {/* Description */}
               <motion.p
-                className="text-gray-300 text-xs text-center leading-relaxed"
+                className="text-gray-300 text-md text-center leading-relaxed"
                 variants={{
-                  hidden: { opacity: 0, y: 10 },
-                  visible: {
+                  rest: { opacity: 0, x: 50 },
+                  hover: {
                     opacity: 1,
-                    y: 0,
-                    transition: { duration: 1.5, ease: "easeOut" },
+                    x: 0,
+                    transition: {
+                      duration: 1.5,
+                      ease: "easeOut",
+                    },
                   },
                 }}
               >
