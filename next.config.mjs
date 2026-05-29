c380c99b51d8250fb4fad2b5f9f3c0dab21ca686
Smedegaardx <anan0013@stud.kea.dk>
2026-05-29 05:41:55 +0200

    Testing images

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "night-club-api-2026-u759.onrender.com",
-        pathname: "/**",
       },
     ],
   },
